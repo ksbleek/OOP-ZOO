2bdda1e800b654cdd86badff32e64262ef28c2dd
ksbleek <kyle.s.bleeker@gmail.com>
2024-06-12 14:13:30 -0500

    ran into issues while testing

---
+++ zoo_manager.py
@@ -0,0 +1,15 @@
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def speak(self):
+        print(f"Animal sound")
+
+class Mammal:
+    def __init__(self, name, species) -> None:
+        super.__init__(name, species)
+    
+    def give_birth(self):
+        print(f"{self.name} the {self.species} has given birth")
+
